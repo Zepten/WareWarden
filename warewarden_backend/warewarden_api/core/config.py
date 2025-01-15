02ca7e83e2d2a8771566d94ba8c0f21c868c9772
Zepten <39930495+Zepten@users.noreply.github.com>
2025-01-15 19:09:33 +0300

    feat: build DB DSN using components from `.env`

--- warewarden_backend/warewarden_api/core/config.py
+++ warewarden_backend/warewarden_api/core/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic import BaseModel, PostgresDsn
+from pydantic import BaseModel, PostgresDsn, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 ENV_PATH = Path(__file__).parent.parent / ".env"
@@ -12,7 +12,11 @@ class RunSettings(BaseModel):
 
 
 class DbSettings(BaseModel):
-    url: PostgresDsn | None = None
+    username: str = "user"
+    password: SecretStr = SecretStr("password")
+    host: str = "localhost"
+    port: int = 5432
+    path: str = "postgres"
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 5
@@ -24,6 +28,17 @@ class DbSettings(BaseModel):
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
         "pk": "pk_%(table_name)s",
     }
+
+    @property
+    def url(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.username,
+            password=self.password.get_secret_value(),
+            host=self.host,
+            port=self.port,
+            path=self.path,
+        )
 
 
 class ApiV1Settings(BaseModel):
@@ -39,7 +54,9 @@ class ApiSettings(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        extra="ignore",
         env_file=ENV_PATH,
+        env_file_encoding="utf8",
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
@@ -50,3 +67,4 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+print(settings.db.url)
