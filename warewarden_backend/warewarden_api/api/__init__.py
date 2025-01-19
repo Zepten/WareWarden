15c495e09410e9bbc4fcd30a5b860053cd602f4f
Zepten <39930495+Zepten@users.noreply.github.com>
2025-01-19 06:13:47 +0300

    feat: remove `/api/` and `/api/v1/` root routes

--- warewarden_backend/warewarden_api/api/__init__.py
+++ warewarden_backend/warewarden_api/api/__init__.py
@@ -8,10 +8,3 @@ from core.config import settings
 router = APIRouter()
 router.include_router(router=api_v1_router, prefix=settings.api.v1.prefix)
 router.include_router(router=health_router, prefix=settings.api.health)
-
-
-@router.get("/", tags=["API root"])
-async def root():
-    return {
-        "message": "WareWarden API",
-    }
