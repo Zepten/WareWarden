36813b6b01654cc63531ea4cb7d197d94cb4ea65
Zepten <39930495+Zepten@users.noreply.github.com>
2025-01-23 07:51:31 +0300

    chore: setup prettier

--- warewarden_frontend/eslint.config.js
+++ warewarden_frontend/eslint.config.js
@@ -1,18 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.flat["jsx-runtime"],
+      prettierConfig,
+    ],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
@@ -24,5 +35,13 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+  }
 );
